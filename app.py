c45872a4d096bac5b5eb6af47cbbf14324207395
Carlos <solrac.19@gmail.com>
2026-01-05 13:16:11 -0600

    Added app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
+import streamlit as st
+
+# Leer los datos del archivo CSV
+car_data = pd.read_csv('data/vehicles_us.csv')
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir histograma')
+
+build_dispersion = st.checkbox("Construir gráfico de dispersión")
+
+# Lógica a ejecutar cuando se hace clic en el botón
+if build_histogram:
+    # Escribir un mensaje en la aplicación
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un histograma utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+
+if build_dispersion: 
+    st.write('Creación de un gráfico de dispersión')
+
+    # Crear un scatter plot utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de scatter
+    fig = go.Figure(data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Relación entre Odómetro y Precio')
+
+    # Mostrar el gráfico Plotly
+    st.plotly_chart(fig, use_container_width=True)
